eedf2334e7f019300b538ecc43b84f2bae1b877f
antoha2503 <114880081+antoha2503@users.noreply.github.com>
2026-07-07 00:12:50 +0300

    Add files via upload

--- vacation/models/vacation_empl.py
+++ vacation/models/vacation_empl.py
@@ -6,29 +6,37 @@ class VacationEmpl(models.TransientModel):
     _description = 'Vacation Empl'
     _rec_name = 'employee_id'
 
-    start_date = fields.Date(string='Start data', required=True, default=fields.Date.today)
-    end_date = fields.Date(string='End data', required=True)
-    number_days = fields.Integer(string='Days', required=True, default=0, readonly=True, compute='_compute_number_days')
+    start_date = fields.Date(string='Початкова дата', required=True, default=fields.Date.today)
+    end_date = fields.Date(string='Кінечна дата', required=True)
+    number_days = fields.Integer(string='Дні', required=True, default=0, readonly=True, compute='_compute_number_days')
     type_statement = fields.Selection([
         ('main_vacation','Заяви на щорічну основну відпустку'),
         ('part_leave','Заяви на частину щорічної відпустки'),
         ('own_account','Заяви на відпустку за власний рахунок'),
         ('birth_child','Заява про відпустку при народженні дитини'),
         ('babysitting','Заява на відпустку для догляду за дитиною'),
-    ], default='main_vacation', required=True)
+    ], default='main_vacation', string="Тип заяви", required=True)
 
-    employee_id = fields.Many2one('hr.employee', string='Employee', readonly=True, default=lambda self: self.env.user.employee_id)
-    name_company = fields.Many2one('res.company', string='Company', related='employee_id.company_id')
-    parent_id = fields.Many2one('hr.employee', string='Parent', readonly=False, default=lambda self: self.env.user.employee_id.parent_id)
-    department_id = fields.Many2one('hr.department', string='Department', related='employee_id.department_id')
+    employee_id = fields.Many2one('hr.employee', string='Співробітник', readonly=True, default=lambda self: self.env.user.employee_id)
+    name_company = fields.Many2one('res.company', string='Назва компанії', related='employee_id.company_id')
+    parent_id = fields.Many2one('hr.employee', string='Керівник', readonly=False, required=True, compute='_onchange_employee_id')
+    department_id = fields.Many2one('hr.department', string='Відділ', required=True, related='employee_id.department_id')
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        for record in self:
+            if record.employee_id and record.employee_id.parent_id:
+                record.parent_id = record.employee_id.parent_id
+            else:
+                record.parent_id = False
 
     @api.constrains('start_date', 'end_date')
     def _check_dates(self):
         for record in self:
             if record.end_date < record.start_date:
-                raise ValidationError("Дата окончания отпуска не может быть раньше даты его начала!")
+                raise ValidationError("Дата закінчення відпустки не може бути раніше дати її початку!")
             if record.start_date < fields.Date.today():
-                raise ValidationError("Нельзя оформить отпуск на прошедшую дату! Выберите сегодняшний день или будущие даты.")
+                raise ValidationError("Не можна оформити відпустку на минулу дату! Виберіть сьогоднішній день або майбутні дати.")
 
     @api.depends('start_date', 'end_date')
     def _compute_number_days(self):
